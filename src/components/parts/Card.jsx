f321692a9cf7caca8c4789503271f9128cd81daf
mauveyone <lennart@muenter.eu>
2023-12-10 11:29:40 +0100

    fixed view for tablets

--- src/components/parts/Card.jsx
+++ src/components/parts/Card.jsx
@@ -4,14 +4,14 @@ function Card(props) {
             <div class="h-full w-full p-4 xl:p-6">
                 <div class="flex flex-col h-full">
                     <div class="flex flex-row text-fullbright text-xl md:text-3xl text-left items-center font-extrabold whitespace-pre-line">
-                        <div class="text-[28px] md:text-[36px] xl:text-[48px] mr-4 md:mr-6 text-prime">
+                        <div class="text-[28px] md:text-[36px] xl:text-[48px] mr-4 xl:mr-6 text-prime">
                             {props.icon || <div></div>}
                         </div>
                         {props.title}
                     </div>
                     <div class="mt-4 text-prime">
                         <div class="">&lth3&gt</div>
-                        <div class="flex flex-row text-fullbright text-md xl:text-xl md:border-l border-prime md:px-4 md:mx-4 md:my-1">
+                        <div class="flex flex-row text-fullbright text-md xl:text-xl md:border-l border-prime md:pl-4 md:mx-4 md:my-1">
                             {props.text}
                         </div>
                         <div class="">&lt/h3&gt</div>
